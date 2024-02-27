021eb42d976043938ac4229ee89d928f0de5da02
vladimir <vladimirmuratov@mail.ru>
2024-02-27 19:27:00 +0300

    fix bug

--- config/defaultValues.js
+++ config/defaultValues.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:3000'
-// export const BASE_URL = 'https://next-auth-prisma-chakra.vercel.app'
+// export const BASE_URL = 'http://localhost:3000'
+export const BASE_URL = 'https://socialnetwork-zeta.vercel.app'
